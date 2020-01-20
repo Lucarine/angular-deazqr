099ce9f1be3322c08ed12c264f521a3368e5ad2d
Lucarine <llliangwenjun193@gmail.com>
2020-01-20 14:12:01 +1100

    3 files modified, 3 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn:"root"
 })
 export class CartService {
   items = [];
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   addToCart(product)
   {
@@ -22,5 +28,8 @@ export class CartService {
     return this.items;
   }
 
-  constructor() { }
+  getShippingPrices()
+  {
+    return this.http.get('/assets/shipping.json');
+  }
 }
